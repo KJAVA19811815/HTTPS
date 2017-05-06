dbe6bcfc4472a50e382b2ccdac1549b043e174c7
KJAVA19811815 <karunsingh1993@gmail.com>
2017-05-06 22:11:44 +0000

    second commit

--- https_example.js
+++ https_example.js
@@ -3,12 +3,18 @@ var https = require('https');
 console.log('i did it');
 
 var options = {
-  host: 'www.example.org',
-  path: '/'
+  host: 'stream-large-file.herokuapp.com',
+  path: '/give-me-stuff-now'
 };
 
-var callback = function() {
+var callback = function(response) {
   console.log('in response handler callabck');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
+
 
 }
 console.log('i m abt to make request');
